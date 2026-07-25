0c682139b0c8572b5c062ced635671d5dc6d8486
marciozenaide <marciozenaide@yahoo.com.br>
2026-07-24 22:51:46 -0300

    chore: atualiza usuario de conexao JDBC para banco carros

--- src/br/com/carros/dao/BaseDAO.java
+++ src/br/com/carros/dao/BaseDAO.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class BaseDAO {
 	
 	private static final String URL = "jdbc:mysql://localhost:3306/carros";
-	private static final String USUARIO = "livro";
-	private static final String SENHA = "livro123";
+	private static final String USUARIO = "carro";
+	private static final String SENHA = "carro123";
 
 	protected Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USUARIO, SENHA);
