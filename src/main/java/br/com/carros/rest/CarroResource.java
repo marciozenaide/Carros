1aba698fd316260c59dd0b24915ffc5b4aa41a6d
marciozenaide <marciozenaide@yahoo.com.br>
2026-09-08 13:55:31 -0300

    refactor: improve CarroResource and register JacksonFeature

--- src/main/java/br/com/carros/rest/CarroResource.java
+++ src/main/java/br/com/carros/rest/CarroResource.java
@@ -2,15 +2,23 @@ package br.com.carros.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.carros.model.Carro;
 import br.com.carros.service.CarroService;
 
 @Path("/carros")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CarroResource {
 	
 	private final CarroService carroService;
@@ -24,9 +32,44 @@ public class CarroResource {
     }
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Carro> findAll() {
 	    return carroService.findAll();
 	}
+	
+	@GET
+	@Path("{id}")
+	public Carro get(@PathParam("id") Long id) {
+		return carroService.findById(id).orElse(null);
+	}
+	
+	@GET
+	@Path("/tipo/{tipo}")
+	public List<Carro> getByTipo(@PathParam("tipo") String tipo){
+		return carroService.findByTipo(tipo);
+	}
+	
+	@GET
+	@Path("/nome/{nome}")
+	public List<Carro> getByNome(@PathParam("nome") String nome){
+		return carroService.findByName(nome);
+	}
 
+	@DELETE
+	@Path("{id}")
+	public Response delete(@PathParam("id") Long id) {
+		carroService.delete(id);
+		return Response.ok("Carro deletado com sucesso").build();
+	}
+	
+	@POST
+	public Response post(Carro carro) {
+		carroService.save(carro);
+		return Response.ok("Carro salvo com sucesso").build();
+	}
+	
+	@PUT
+	public Response put(Carro carro) {
+		carroService.save(carro);
+		return Response.ok("Carro atualizado com sucesso").build();
+	}
 }
--- src/main/java/br/com/carros/rest/RestApplication.java
+++ src/main/java/br/com/carros/rest/RestApplication.java
@@ -2,6 +2,7 @@ package br.com.carros.rest;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/api")
@@ -9,5 +10,6 @@ public class RestApplication extends ResourceConfig {
 
 	public RestApplication() {
 		packages("br.com.carros.rest");
+		register(JacksonFeature.class); // <--- ADICIONE ESTA LINHA
 	}
 }
